63a0be5edc2160db4e3db96e259d151b96646537
KingTingTheGreat <jting@bu.edu>
2023-06-03 13:13:33 -0400

    allows user to legofy an image on local machine

---
+++ main.py
@@ -0,0 +1,8 @@
+from Converter import Converter
+from LegofiedImage import LegofiedImage, LEGO_COLORS, BRICKLINK_COLORS, PART_NUMBERS
+
+c = Converter()
+path = input('Enter path to image: ')
+image:LegofiedImage = c.convert(path, 16*5, progress_bar=True, use_lego=False)
+image.save_image()
+image.draw()
