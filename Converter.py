bb6e58a9554481c487290d4e94f65ff2f98ea3e2
KingTingTheGreat <jting@bu.edu>
2023-11-26 19:40:47 -0500

    removed tqdm from package requirements

--- Converter.py
+++ Converter.py
@@ -1,40 +1,44 @@
 import numpy as np
 from PIL import Image
-from tqdm import tqdm
+
+# from tqdm import tqdm
 from pillow_heif import register_heif_opener
 from LegofiedImage import LegofiedImage, LEGO_COLORS, BRICKLINK_COLORS, PART_NUMBERS
 
+
 class Converter:
-
     def __init__(self):
-        self.colors:dict[str, str] = LEGO_COLORS
-        self.progress_bar:bool = False
+        self.colors: dict[str, str] = LEGO_COLORS
+        self.progress_bar: bool = False
 
-    def find_closest_color(self, target:str) -> str:
-        """ returns key corresponding to closest color to target in self.colors """
-        closest_color:str = ''
-        closest_distance:float = 500  # two colors can't be more than 441 apart
+    def find_closest_color(self, target: str) -> str:
+        """returns key corresponding to closest color to target in self.colors"""
+        closest_color: str = ""
+        closest_distance: float = 500  # two colors can't be more than 441 apart
         for color in self.colors:
-            distance:float = self.color_distance(color, target)
+            distance: float = self.color_distance(color, target)
             if distance < closest_distance:
                 closest_color = color
                 closest_distance = distance
         return closest_color
-    
-    def color_distance(self, color1:str, color2:str) -> float:
-        """ returns the euclidean distance between two hexcode colors """
-        color1 = np.array([int(color1[i:i+2], 16) for i in range(1, 6, 2)])
-        color2 = np.array([int(color2[i:i+2], 16) for i in range(1, 6, 2)])
+
+    def color_distance(self, color1: str, color2: str) -> float:
+        """returns the euclidean distance between two hexcode colors"""
+        color1 = np.array([int(color1[i : i + 2], 16) for i in range(1, 6, 2)])
+        color2 = np.array([int(color2[i : i + 2], 16) for i in range(1, 6, 2)])
         diff = color1 - color2
         return np.sqrt(np.dot(diff.T, diff))
-    
-    def _convert(self, image, length, resampling_func=Image.Resampling.BILINEAR) -> list[list[str]]:
+
+    def _convert(
+        self, image, length, resampling_func=Image.Resampling.BILINEAR
+    ) -> list[list[str]]:
         l, h = image.size
-        height = int((length/l)*h)
+        height = int((length / l) * h)
 
         im = image.resize((length, height), resample=resampling_func)
 
-        iter = range(height) if not self.progress_bar else tqdm(range(height))
+        # iter = range(height) if not self.progress_bar else tqdm(range(height))
+        iter = range(height)
         results = []
         for i in iter:
             row = []
@@ -44,19 +48,33 @@ class Converter:
                     pix = (pix, pix, pix)
                 if len(pix) > 3:
                     pix = pix[:3]
-                row.append(self.find_closest_color('#%02x%02x%02x' % pix))
+                row.append(self.find_closest_color("#%02x%02x%02x" % pix))
             results.append(row)
 
         return results
 
-    def convert_file(self, path:str, length:int, progress_bar=False, use_lego=True, resampling_func=Image.Resampling.BILINEAR) -> LegofiedImage:
+    def convert_file(
+        self,
+        path: str,
+        length: int,
+        progress_bar=False,
+        use_lego=True,
+        resampling_func=Image.Resampling.BILINEAR,
+    ) -> LegofiedImage:
         self.colors = LEGO_COLORS if use_lego else BRICKLINK_COLORS
         self.progress_bar = progress_bar
-        with Image.open(path, mode='r') as image:
+        with Image.open(path, mode="r") as image:
             results = self._convert(image, length, resampling_func=resampling_func)
-        return LegofiedImage(results, self.colors, path.split('/')[-1])
+        return LegofiedImage(results, self.colors, path.split("/")[-1])
 
-    def convert_image(self, image:Image, length:int, progress_bar=False, use_lego=True, resampling_func=Image.Resampling.BILINEAR) -> LegofiedImage:
+    def convert_image(
+        self,
+        image: Image,
+        length: int,
+        progress_bar=False,
+        use_lego=True,
+        resampling_func=Image.Resampling.BILINEAR,
+    ) -> LegofiedImage:
         self.colors = LEGO_COLORS if use_lego else BRICKLINK_COLORS
         self.progress_bar = progress_bar
         results = self._convert(image, length, resampling_func=resampling_func)
